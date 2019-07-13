0bb35efe258bdace73215c6f77e23a594e94238d
Emmanuel Plummer <emmanuelplummer@gmail.com>
2019-07-13 02:27:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ function logWhisper (string){
 
 function sayHiToGrandma (string){
   if(string === "I love you, Grandma.") return ("I love you, too.") 
-  else if (string === string.toLowerCase()) return ()
-  
-  
-  
+  else if (string === string.toLowerCase()) return ('I can\'t hear you!')
+  else if (string === string.toUpperCase()) return ('YES INDEED!')
 }
