c76c9486569ea098cfc5e1302e3ff6fb89cdc9ef
Emmanuel Plummer <emmanuelplummer@gmail.com>
2019-07-13 02:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper (string){
 }
 
 function sayHiToGrandma (string){
- if (sayHiToGrandma(string) === whisper); return ("I can\'t hear you!")
+ if (string) === whisper); return ("I can\'t hear you!")
 }
 
 function sayHiToGrandma (string){
